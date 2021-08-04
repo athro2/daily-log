1405ea671273630e2d0c1000acd51d52f827c188
ugangdan <84281591+ugangdan@users.noreply.github.com>
2021-08-04 22:06:53 +0900

    Update always 1.py

--- always 1.py
+++ always 1.py
@@ -4,3 +4,13 @@ print(6 ** 7 - 279935) #제곱
 print(6-20/4)
 print(21%5) #나머지
 print(5//3) #몫
+print(abs(-1)) #절댓값
+print(pow(1, 8)) #제곱 1^8
+print(max((-10), 1)) #큰값
+print(min(1, 8)) #작은값
+print(round(1.34)) #반올림
+
+from math import*
+print(floor(1.78)) #내림
+print(ceil(1.12)) #올림
+print(sqrt(1)) #제곱근
