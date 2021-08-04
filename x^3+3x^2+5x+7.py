aa37cc3a42392ec781cbbcc2f47c1c2a03b991ce
ugangdan <84281591+ugangdan@users.noreply.github.com>
2021-08-04 19:13:53 +0900

    Update x^3+3x^2+5x+7.py

--- x^3+3x^2+5x+7.py
+++ x^3+3x^2+5x+7.py
@@ -1,6 +1,9 @@
 number = 7
+
 answer = ((number ** 3) + 3 * (number ** 2 ) + 5 * (number) + 7)
 print(str(answer))
 answer1 = ((number ** 3) + 3 * (number ** 2 ) + 5 * (number))
 number = answer1 + 7
 print(str(number))
+answer1 += 7
+print(answer1)
