59ec50c31b180515a01b9746a6ae52c3849d1dc1
ugangdan <84281591+ugangdan@users.noreply.github.com>
2021-08-04 17:12:28 +0900

    Update THE GAME.py

--- THE GAME.py
+++ THE GAME.py
@@ -7,4 +7,4 @@ win = answer1 > answer2
 print("길을 가던 " + people1 + "와 " + people2 + "는 사나이의 자존심을 걸고 숫자놀이를 하기로 하였다")
 print("숫자놀이란 더 큰 숫자를 내는 사람이 이기는 경기였다")
 print("" + people1 + "는 " + str(answer1) + "을 냈고 " + people2 + "는 " + str(answer2) + "을 냈다")
-print("Q: " + people1 + "는 " + people2 + "를 이겼다. 사실일까?: A: " + str(win))
+print("Q: " + people1 + "는 " + people2 + "를 이겼다. 사실일까? A: " + str(win))
